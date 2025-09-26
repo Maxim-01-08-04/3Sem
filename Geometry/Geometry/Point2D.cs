9ee9f97bf230326ed5f1aeeb4fb475f763095c67
stevered <stevered@student.21-school.ru>
2025-09-26 22:22:49 +0700

    Пока что не фулл комплит

---
+++ Geometry/Geometry/Point2D.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Point2D
+    {
+        private int X;
+        private int Y;
+
+        public Point2D(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int getX()
+        {
+            return X;
+        }
+
+        public int getY()
+        {
+            return Y;
+        }
+
+        public void addX(int x)
+        {
+            X += x;
+        }
+
+        public void addY(int y)
+        {
+            Y += y;
+        }
+
+        public void setX(int x)
+        {
+            X = x;
+        }
+
+        public void setY(int y)
+        {
+            Y = y;
+        }
+    }
+}
