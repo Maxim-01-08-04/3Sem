94b3f91caf7a313ebda43c99ad6e587e4db46443
Maksim <160127575+Maxim-01-08-04@users.noreply.github.com>
2025-11-01 08:25:44 +0700

    Add files via upload

---
+++ InputDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Geometry
+{
+    public partial class InputDialog : Window
+    {
+        public string Answer { get; set; }
+
+        //public InputDialog(string question, string defaultAnswer = "")
+        //{
+        //    InitializeComponent();
+        //    lblQuestion.Content = question;
+        //    txtAnswer.Text = defaultAnswer;
+        //}
+
+        //private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Answer = txtAnswer.Text;
+        //    DialogResult = true;
+        //}
+
+        //private void Window_ContentRendered(object sender, System.EventArgs e)
+        //{
+        //    txtAnswer.SelectAll();
+        //    txtAnswer.Focus();
+        //}
+
+        private void y_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MessageBox.Show("ksdjfhks");
+        }
+
+        private void x_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MessageBox.Show("ksdjfhks");
+
+        }
+    }
+}
